8f1237cf229ebc23db17616697b7bd23992464a8
bcheema007 <acheema@mail.greenriver.edu>
2021-02-26 05:59:04 -0800

    Added Cyclops back into the game and commented out the line in maps.js which spawned him next to Rutherford. Added in a table to index html which describes the various game controls available. Made two other minor adjustments in index.html

--- core/scenemanager.js
+++ core/scenemanager.js
@@ -114,7 +114,7 @@ class SceneManager {
                         this.game.addEntity(new Fayere(this.game, sx * 32 * scale, sy * 32 * scale));
                     }
                 } else if (e[0] === "cyclops") {
-                    var enemy = new Slippey(this.game, Math.floor(r.x + r.w/2) * 32 * scale, Math.floor(r.y + r.h/2) * 32 * scale);
+                    var enemy = new Cyclops(this.game, Math.floor(r.x + r.w/2) * 32 * scale, Math.floor(r.y + r.h/2) * 32 * scale);
                     this.game.addEntity(enemy);
                 } else if (e[0] === "buck") {
                     this.game.addEntity(new Buck(this.game, Math.floor(r.x + r.w/2) * 32 * scale, Math.floor(r.y + r.h/2) * 32 * scale));
